2fd334acc980d04e250ba12f529d1bc2923b7cbe
yazan <yazan@LTUCSTUDENT-L048.localdomain>
2021-05-10 20:50:09 +0300

    lab 13 finish

--- src/test/java/com/example/songr/SongrApplicationTests.java
+++ src/test/java/com/example/songr/SongrApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.songr;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SongrApplicationTests {
+    @Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	void homePageTest() throws Exception {
+		mockMvc
+				.perform(get("/"))
+				.andDo(print())
+				.andExpect(status().isOk());
 	}
-
 }
